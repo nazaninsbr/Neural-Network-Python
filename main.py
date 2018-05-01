06f477920b085faab02a9736d98bfa9f969267c2
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-05-01 21:50:11 +0430

    start new project

---
+++ main.py
@@ -0,0 +1,6 @@
+def readImageFile(file_name):
+
+
+if __name__ == '__main__':
+	file_name = input('File Name: ')
+	image = readImageFile(file_name)
---
+++ neuron.py
@@ -0,0 +1,4 @@
+class Neuron:
+	def __init__(self, pixel_value, weight):
+		self.pixel_value = pixel_value
+		self.weight = weight
